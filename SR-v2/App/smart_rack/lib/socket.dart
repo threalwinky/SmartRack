93e08df5c96cf5af368e909bb2de0b07af70029f
threalwinky <voquangvu09112006@gmail.com>
2023-09-30 02:35:07 +0700

    upload files

--- SR-v2/App/smart_rack/lib/socket.dart
+++ SR-v2/App/smart_rack/lib/socket.dart
@@ -1,6 +1,6 @@
 import 'package:json_annotation/json_annotation.dart';
 
-part './temphumi.h.dart';
+part 'socket.h.dart';
 
 @JsonSerializable(explicitToJson: true)
 class TempHumi {
--- SR-v2/App/smart_rack/lib/socket.h.dart
+++ SR-v2/App/smart_rack/lib/socket.h.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'temphumi.dart';
+part of 'socket.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
